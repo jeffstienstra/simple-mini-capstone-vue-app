fc144bad303648525db5ec4fb6fa08181aa9fd9b
jeffstienstra <jeffstienstra@gmail.com>
2021-05-31 20:59:36 -0400

    add web request to localhost 3000 simple-mini-capstone

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>All Products</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>{{ product.name }}</h2>
+      <p>Price: {{ product.price }}</p>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to your homepage!",
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products").then((response) => {
+        this.products = response.data;
+        console.log("All Products", this.products);
+      });
+    },
+  },
 };
 </script>
